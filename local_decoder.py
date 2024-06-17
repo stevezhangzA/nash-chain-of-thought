72f8441657ae7967e3c00f50a8cb9e4c4432ede0
Stephen Zhang <166477543+stevezhangzA@users.noreply.github.com>
2024-06-18 07:32:22 +0800

    Update local_decoder.py

--- local_decoder.py
+++ local_decoder.py
@@ -1,10 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import pickle
-try:
-    from modelscope import Model, snapshot_download
-    from modelscope.models.nlp.llama2 import Llama2Tokenizer
-except:
-    pass
+
 import torch
 
 class custom_api(object):
