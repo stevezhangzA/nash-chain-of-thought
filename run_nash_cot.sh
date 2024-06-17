5e2f9494ca9386339ba594691af635f9abd397a2
Stephen Zhang <166477543+stevezhangzA@users.noreply.github.com>
2024-06-18 07:31:37 +0800

    Update run_nash_cot.sh

--- run_nash_cot.sh
+++ run_nash_cot.sh
@@ -1,1 +1,8 @@
-python nash_cot.py --method zero_shot_cot --dataset $1 --random_seed $2 --sub_dir template_research --tokenizer_path $3 --model_tag $3 --inner_loop $4 --outer_loop $5
+python nash_cot.py --method zero_shot_cot \
+                   --dataset $1 \
+                   --random_seed $2 \
+                   --sub_dir experiments \
+                   --tokenizer_path $3 \
+                   --model_tag $3 \
+                   --inner_loop $4 \
+                   --outer_loop $5
